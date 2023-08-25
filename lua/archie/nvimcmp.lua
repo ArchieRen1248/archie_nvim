c8d2f54b9ee6ee6af7fff7bd636226124b08f613
ArchieRen1248 <yuren1248@qq.com>
2023-08-25 22:33:00 +0800

    Add support to cmdline auto completion.

--- lua/archie/nvimcmp.lua
+++ lua/archie/nvimcmp.lua
@@ -64,6 +64,24 @@ cmp.setup {
   },
 }
 
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
